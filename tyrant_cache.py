4c1c7635be16a55b052ea09392e7c194494e21d8
Chris Stucchio <stucchio@gmail.com>
2011-08-06 11:18:53 +0530

    Bug: if the tokyo tyrant server is reset, tyrant cache fails to work in the future. Fix: if we encounter a socket error, we reset the connection.

--- tyrant_cache.py
+++ tyrant_cache.py
@@ -5,20 +5,37 @@ except ImportError:
 
 import pytyrant
 import time
+import socket
 
 from django.core.cache.backends.base import BaseCache
 from django.utils.encoding import smart_unicode, smart_str
+
+def __retry_with_reset_on_error__(func):
+    def wrapped(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except socket.error, e:
+            self.reset_cache_connection()
+            return func(self, *args, **kwargs)
+    return wrapped
+
 
 class CacheClass(BaseCache):
     def __init__(self, server, params):
         "Connect to Tokyo Tyrant, and set up cache backend."
         BaseCache.__init__(self, params)
         host, port = server.split(':')
-        self._cache = pytyrant.Tyrant.open(host, int(port))
+        self.host = host
+        self.port = port
+        self._cache = pytyrant.Tyrant.open(self.host, int(self.port))
+
+    def reset_cache_connection(self):
+        self._cache = pytyrant.Tyrant.open(self.host, int(self.port))
 
     def _prepare_key(self, raw_key):
         return smart_str(raw_key)
 
+    @__retry_with_reset_on_error__
     def add(self, key, value, timeout=0):
         "Add a value to the cache. Returns ``True`` if the object was added, ``False`` if not."
         try:
@@ -28,24 +45,28 @@ class CacheClass(BaseCache):
             return False
         return True
 
+    @__retry_with_reset_on_error__
     def get(self, key, default=None):
         "Retrieve a value from the cache. Returns unpicked value if key is found, 'default' if not. "
         try:
             value = self._cache.get(self._prepare_key(key))
         except pytyrant.TyrantError:
             return default
+
         value = pickle.loads(value)
         if isinstance(value, basestring):
             return smart_unicode(value)
         else:
             return value
 
+    @__retry_with_reset_on_error__
     def set(self, key, value, timeout=0):
         "Persist a value to the cache."
         value = pickle.dumps(value)
         self._cache.put(self._prepare_key(key), value)
         return True
 
+    @__retry_with_reset_on_error__
     def delete(self, key):
         "Remove a key from the cache."
         try:
@@ -53,14 +74,17 @@ class CacheClass(BaseCache):
         except pytyrant.TyrantError: #Should not raise error if key doesn't exist
             return None
 
+    @__retry_with_reset_on_error__
     def get_many(self, keys):
         "Retrieve many keys."
         many = self._cache.mget(keys)
         return [{k:pickle.loads(v)} for k,v in many]
 
+    @__retry_with_reset_on_error__
     def flush(self, all_dbs=False):
         self._cache.vanish()
 
+    @__retry_with_reset_on_error__
     def incr(self, key, delta=1):
         "Atomically increment ``key`` by ``delta``."
         return self._cache.addint(self._prepare_key(key), delta)
